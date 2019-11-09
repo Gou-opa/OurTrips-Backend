f0b82d2706ed32ca5b97646b62763a21a2541f03
Manh Tien <manhtiennp.97@gmail.com>
2019-11-09 14:28:23 +0700

    login nhe

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.all('/ping', function (req, res) {
-   res.json({"OurTrips" : "PONG"});
+
+router.get('/ping', function(req, res, next) {
+	res.render('./login/app.ejs', { pageID: 1});
 });
+
+router.get('/register', function(req, res, next) {
+	res.render('./login/app.ejs', { pageID: 2});
+})
+
+router.get('/forgot-password', function(req, res, next) {
+	res.render('./login/app.ejs', { pageID: 0});
+})
+
+
 module.exports = router;
