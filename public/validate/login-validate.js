953992e2787f65d477f1feb15aebf23c32527381
Manh Tien <manhtiennp.97@gmail.com>
2019-11-22 13:32:31 +0700

    validate

---
+++ public/validate/login-validate.js
@@ -0,0 +1,129 @@
+$("#login_form").validate({
+    rules: {
+        username: {
+            required: true
+        },
+        password: {
+            required: true
+        }
+    },
+    messages: {
+        username: {
+            required: "* Bạn chưa điền Email"
+        },
+        password: {
+            required: "* Bạn chưa điền password"
+        }
+    },
+    errorPlacement: function (label, elem) {
+        var name = elem.attr('name');
+        var spanError = '#' + name;
+        $(spanError).append(label.addClass('text-error'));
+    },
+});
+
+$("#register_form").validate({
+    rules: {
+        username: {
+            required: true,
+            maxlength: 18,
+            minlength: 6,
+        },
+        name : {
+            required: true,
+        },
+        gender: {
+            required: true,
+        },
+        birthday: {
+            required: true,
+        },
+        address: {
+            required: true,
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        tel: {
+            required: true,
+            number: true
+        },
+        nationality: {
+            required: true,
+        },
+        password: {
+            required: true,
+            min: 6
+        },
+        password_confirmation: {
+            required: true,
+            equalTo: '#password',
+        },
+
+    },
+    messages: {
+        username: {
+            required: "* Bạn chưa điền Username",
+            maxlength: "* Nhập tối đa 18 kí tự",
+            minlength: "* Nhập tối thiểu 6 kí tự"
+        },
+        name: {
+            required: "* Bạn chưa điền họ tên"
+        },
+        gender: {
+            required: "* Bạn chưa điện giới tính",
+        },
+        birthday: {
+            required: "* Bạn chưa điền ngày sinh",
+        },
+        address: {
+            required: "* Bạn chưa điền địa chỉ",
+        },
+        email: {
+            required: "*Bạn chưa điền email",
+            email: "* Email chưa đúng định dạng",
+        },
+        tel: {
+            required: "* Bạn chưa nhập số điện thoại",
+            number: "* Bạn phải nhập đúng định dạng"
+        },
+        nationality: {
+            required: "* Bạn chưa nhập quốc tịch",
+        },
+        password: {
+            required: "* Bạn chưa điền password",
+            minlength: "Nhập tối thiểu 6 kí tự"
+        },
+        password_confirmation: {
+            required: "* Bạn chưa nhập lại mật khẩu",
+            equalTo: "* Mật khẩu không trùng khớp",
+        }
+    },
+    errorPlacement: function (label, elem) {
+        var name = elem.attr('name');
+        var spanError = '#' + name;
+        $(spanError).append(label.addClass('text-error'));
+    },
+});
+
+$('#forgotpassowrd_form').validate({
+    rules: {
+        email_registration: {
+            required: true,
+            email: true
+        }
+    },
+    messages: {
+      email_registration: {
+          required: "* Bạn cần phải nhập email đã đăng ký",
+          email: "* Email bạn nhập sai định dạng",
+      }
+    },
+    errorPlacement: function (label, elem) {
+        var name = elem.attr('name');
+        var spanError = '#' + name;
+        $(spanError).append(label.addClass('text-error'));
+    },
+});
+
